a8eb58a663133bea4895f5373b022b320fd1ed6c
Reshma <reshma.s@iorbit-tech.com>
2023-01-28 09:40:06 +0530

    internalIorbit

--- src/Components/CreateForm/Validattion.js
+++ src/Components/CreateForm/Validattion.js
@@ -1,12 +1,15 @@
 import { TextField } from "@mui/material";
 
 export const validation = (comp, type) => {
-    console.log("from validate ",comp,type);
+    console.log("from validate ", comp, type);
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let phone = /^(\+\d{1,3}[- ]?)?\d{10}$/;
     let alphanumeric = /^[a-z0-9\s,'-]*$/;
-    let price =  /^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/; 
-   
+    let price = /^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/;
+    let aadhar = /^\d{4}\s\d{4}\s\d{4}$/;
+    let pan = /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/;
+    
+
     if (type == 'email') {
         if (comp.match(regex))
             return true;
@@ -29,11 +32,24 @@ export const validation = (comp, type) => {
             return true;
         else
             return false;
-    } else {
+    }
+    else if (type == 'aadhar') {
+        if (comp.match(aadhar))
+            return true;
+        else
+            return false;
+    }
+    else if (type == 'pan') {
+        if (comp.match(pan))
+            return true;
+        else
+            return false;
+    }
+    else {
         return true;
     }
-  
-   
-   
+
+
+
 }
 
